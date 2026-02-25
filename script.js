8d2aaa432447501129075478a016bd1eaba74f02
johannady2 <johannady2@gmail.com>
2026-02-26 08:41:24 +0900

    used lodash without function and console logged

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var _ = require('lodash');
 console.log(_);
 
 var array = [1,2,3,4,5,6,7,8];
-console.log('answer:', _.without);
+console.log('answer:', _.without(array,3));
 let h3tags = document.querySelector("h3");
 let color1 = document.querySelector("#color1");
 let color2 = document.querySelector("#color2");
